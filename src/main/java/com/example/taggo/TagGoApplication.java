e361cd821a17dc6b0492eec11ba0c4b4f4403158
SD-gif <alem6516@naver.com>
2025-04-30 14:59:20 +0900

    initial project setting

---
+++ src/main/java/com/example/taggo/TagGoApplication.java
@@ -0,0 +1,13 @@
+package com.example.taggo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TagGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taggo/TagGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taggo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
